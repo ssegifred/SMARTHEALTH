494ef875af5ccd8739c6330d0186e57853420adc
MUWANGUZI JOVIC BIRALO <95884686+BIRALO256@users.noreply.github.com>
2023-10-03 15:35:39 +0300

    improved welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,82 +1,93 @@
-@extends('layouts.app')
+@extends('layouts.app1')
 
 @section('content')
-    <div class="container">
-        <div class="row mb-4 justify-content-center">
-            <div class="col-md-8">
-              
-               
-                @guest
-                    <div class="mt-5">
-                        <a href="{{ url('/register') }}"> <button class="btn btn-primary">Register as Patient</button></a>
-                        <a href="{{ url('/login') }}"><button class="btn btn-success">Login</button></a>
-                    </div>
-                @endguest
+
+<!-- Welcome Section -->
+<section class="container mt-5">
+    <div class="row">
+        <div class="col-md-6">
+            <h1>Welcome to Our Doctor Appointment Website</h1>
+            <p>Book your next doctor's appointment with ease.</p>
+            <p>We are available 24/7 to assist you with your healthcare needs.</p>
+        </div>
+        <div class="col-md-6">
+            <div class="text-center">
+                <!-- Add your image or logo here with a maximum height for larger screens -->
+                <img src="logo/pharmacy.jpg" alt="Website Logo" class="img-fluid" style="max-height: 200px;">
             </div>
         </div>
+    </div>
+</section>
 
-        {{-- Input --}}
-        <form action="{{ url('/') }}" method="GET">
-            <div class="card">
-                <div class="card-body">
-                    <div class="card-header">Find Doctors</div>
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-md-6 col-sm-8">
-                                <input type="date" name='date' id="datepicker" class='form-control'>
-                            </div>
-                            <div class="col-md-6 col-sm-4">
-                                <button class="btn btn-primary" type="submit">Go</button>
-                            </div>
+<!-- About Us Section -->
+<section class="bg-light">
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>About Us</h2>
+                <p>We are a dedicated team of healthcare professionals committed to making the process of booking doctor appointments as convenient as possible. Our mission is to ensure that you receive the best healthcare services without any hassle.</p>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
+<!-- Image Carousel Section -->
+<section class="container mt-5">
+    <div class="row">
+        <div class="col-md-12">
+            <div id="imageCarousel" class="carousel slide" data-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="expect/doctor2.avif" class="d-block w-100" alt="Image 1 " style="max-height: 600px;">
+                        <div class="carousel-caption">
+                            <h3 class="text-danger p-2">Access to a network of qualified healthcare providers</h3
+                            <p>This is the description for the first image.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="expect/doctor4.avif" class="d-block w-100" alt="Image 2" style="max-height: 600px;">
+                        <div class="carousel-caption">
+                            <h3  class="text-danger p-2">Prescription Refills</h3
+                            <p>Easily request and renew your prescriptions online.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="expect/doctor3.avif" class="d-block w-100" alt="Image 3" style="max-height: 600px;">
+                        <div class="carousel-caption">
+                            <h3 class="text-danger p-2">Easy appointment booking</h3
+                            <p>This is the description for the first image.</p>
+                        </div>
+                    </div>
+                    <div class="carousel-item">
+                        <img src="expect/doctor6.webp" class="d-block w-100" alt="Image 3" style="max-height: 600px;">
+                        <div class="carousel-caption">
+                            <h3 class="text-danger p-2">Specialist Referrals</h3
+                            <p> Get access to specialist doctors when needed.</p>
                         </div>
                     </div>
                 </div>
+                <a class="carousel-control-prev" href="#imageCarousel" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#imageCarousel" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
             </div>
-        </form>
-
-        {{-- Display doctors --}}
-        <div class="card">
-            <div class="card-body">
-                <div class="card-header">List of Doctors Available: @isset($formatDate) {{ $formatDate }}
-                    @endisset
-                </div>
-                <div class="card-body table-responsive-sm">
-                    <table class="table table-striped">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Photo</th>
-                                <th>Name</th>
-                                <th>Expertise</th>
-                                <th>Book</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @forelse ($doctors as $key=>$doctor)
-                                <tr>
-                                    <td>{{ $key + 1 }}</td>
-                                    <td><img src="{{ asset('images') }}/{{ $doctor->doctor->image }}" alt="doctor photo"
-                                            width="100px"></td>
-                                    <td>{{ $doctor->doctor->name }}</td>
-                                    <td>{{ $doctor->doctor->department }}</td>
-                                    @if (Auth::check() && auth()->user()->role->name == 'patient')
-                                        <td>
-                                            <a href="{{ route('create.appointment', [$doctor->user_id, $doctor->date]) }}"><button
-                                                    class="btn btn-primary">Appointment</button></a>
-                                        </td>
-                                    @else
-                                        <td>For patients ONLY</td>
-                                    @endif
-                                </tr>
-                            @empty
-                                <td>No doctors available</td>
-                            @endforelse
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
         </div>
     </div>
+</section>
+
+
+<!-- Include Bootstrap JS -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.3/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+
+
 
 @endsection
